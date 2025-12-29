86adc60e594796c98cddd8daf63101d231499e3d
Michael Roberts <50212290+Necro2772@users.noreply.github.com>
2025-12-29 04:28:36 +0200

    Added tilemap files

---
+++ assets/caveTiles.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="caveTiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="caveTiles.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="Cave Walls" type="corner" tile="-1">
+   <wangcolor name="" color="#303030" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="32" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="33" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="34" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
